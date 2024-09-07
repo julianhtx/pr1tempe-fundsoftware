afced9ed7f23754d76c8e93d82d69619aa58cce8
Julian Trejo <ljht.2004@gmail.com>
2024-09-07 15:27:47 -0600

    se agregó la tabla de usuarios y su procedimiento almacenado

--- databaseTemperaturas.sql
+++ databaseTemperaturas.sql
@@ -1,3 +1,5 @@
+--db: practicatemperatura
+
 --* CREACIÓN DE TABLAS
 CREATE TABLE Users(
     idUser INT PRIMARY KEY AUTO_INCREMENT,
@@ -17,3 +19,29 @@ CREATE TABLE Sensores(
 
 --! CREACIÓN DE CRUD
 --*Validar usuarios
+DROP PROCEDURE IF EXISTS p_ValidarUsuario;
+CREATE PROCEDURE p_ValidarUsuario(
+    IN _userName VARCHAR(255),
+    IN _password VARCHAR(255)
+)
+BEGIN
+    DECLARE x INT;
+	SELECT COUNT(*) FROM Users WHERE userName = _userName AND Password = _pass INTO x;
+	if x > 0 then
+		SELECT 'Correcto' AS rs, (SELECT level FROM Users WHERE userName = _userName) AS Nivel;
+	ELSE
+		SELECT 'Error' AS rs, 0 AS Nivel;
+	END if;
+END;
+
+INSERT INTO Users ( userName, firstName, lastName, level, password ) VALUES(
+     'User1', 'Julian', 'Hernandez', '1', sha1('2004')
+),
+(
+     'User2', 'Luis', 'Trejo', '2', sha1('2004')
+),
+(
+     'User3', 'Juan', 'Perez', '3', sha1('1234')
+);
+
+create view v_Users AS select idUser, userName, password, firstName, lastName, level from Users;
